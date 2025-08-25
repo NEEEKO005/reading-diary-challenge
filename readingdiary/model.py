f07a9a33f9ef8c44ef2c147a3b7fe44a5f8e61a8
Nicolas correa <nicolascorreapulido@gmail.com>
2025-08-25 17:02:11 -0500

    Creacion de la clase Book

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -6,3 +6,22 @@ class Note:
         self.text:str = text
         self.page:int = page
         self.date:datetime = date
+
+    def __str__(self) -> str :
+        return f"{self.date} -page {self.page}:{self.text}"
+
+class Book:
+
+    Excelent:int=3
+    Good:int=2
+    Bad:int=1
+    Unrated:int=0
+
+    def __init__(self, isbn:str, title:str,author:str,page:int):
+        self.isbn:str = isbn
+        self.title:str = title
+        self.author:str = author
+        self.page:int = page
+        self.rating:int = Book.Unrated
+        self.notes:list[Note] = []
+
